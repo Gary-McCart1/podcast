209ba02f8b3e2099882b08f5ee908913e9ffb2fa
Gary McCart <gwmccart3@gmail.com>
2025-02-11 19:36:01 -0500

    Updated podcast app with local JSON support

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+import json
+
+
+app = Flask(__name__)
+
+
+# Open the local JSON file
+with open("./data/data.json", "r") as file:
+    # Parse the JSON data
+    data = json.load(file)
+
+# Now you can work with the `data` like any Python object
+
+@app.route("/")
+def home():
+    return render_template("index.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
