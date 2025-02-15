614e22d5b5b03310dbc29538f16025fb849e0698
Gary McCart <gwmccart3@gmail.com>
2025-02-14 19:02:48 -0500

    Update favicon

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import json
 
 app = Flask(__name__)
 
-
-# # Open the local JSON file
-# with open("./data/data.json", "r") as file:
-#     # Parse the JSON data
-#     data = json.load(file)
-
-# # Now you can work with the `data` like any Python object
-
 @app.route("/")
 def home():
     with open("./data/data.json", "r") as file:
